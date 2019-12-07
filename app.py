d9be6c9acdeed9c33e6759ec5dfcbeb7044ea0ad
Oswaldo Lisboa <47566857+OswaldoLisboa@users.noreply.github.com>
2019-12-07 22:21:49 +0000

    Added a function for every page.

--- app.py
+++ app.py
@@ -27,12 +27,52 @@ def index():
     # User reached route via POST
     else:
         session["user"] = request.form.get("username")
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+        if rows == []:
+            return("WRONG USER")
+
+        if not check_password_hash(rows[0]["pass_hash"], request.form.get("password")):
+            return("WRONG PASSWORD")
+
+        session["user_id"] = rows[0]["user_id"]
         return redirect("/daily_view")
 
 @app.route('/daily_view', methods=["GET", "POST"])
 @login_required
 def daily_view():
-    return("Heeeeeyy")
+    """ TODO """
+    return("TODO")
+
+@app.route('/detailed_view', methods=["GET"])
+@login_required
+def detailed_view():
+    """ TODO """
+    return("TODO")
+
+@app.route('/monthly_view', methods=["GET", "POST"])
+@login_required
+def monthly_view():
+    """ TODO """
+    return("TODO")
+
+@app.route('/yearly_view', methods=["GET", "POST"])
+@login_required
+def yearly_view():
+    """ TODO """
+    return("TODO")
+
+@app.route('/buy', methods=["GET", "POST"])
+@login_required
+def buy():
+    """ TODO """
+    return("TODO")
+
+@app.route('/confirmation', methods=["GET"])
+@login_required
+def confirmation():
+    """ TODO """
+    return("TODO")
 
 
 if __name__ == "__main__":
